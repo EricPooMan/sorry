a46eae2fb7b0e5a929342e511c1a448c078bfd13
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-02-23 17:39:18 +0800

    Add files via upload

--- script/redirect.js
+++ script/redirect.js
@@ -1,9 +1,12 @@
 var web_url = window.location.href
 var url = new URL(web_url)
 var redirectLink = url.searchParams.get("redirect")
+if (redirectLink == "uA3j2Ko2afC6jsF3hiH7AfOk62nd51f3i9web"){
+    moveURL = "http://www.youtube.com"
+}
 checkRecap = true
 while (checkRecap){
-    if(grecaptcha.getResponse().length == 484){
-        console.log("The captcha has been already solved");
+    if(! grecaptcha.getResponse().length == 0){
+        window.location.href = moveURL
     }
 }
