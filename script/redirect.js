749a387a27f92e9a254f902f5b089cea5068fe13
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-02-23 17:19:46 +0800

    Add files via upload

--- script/redirect.js
+++ script/redirect.js
@@ -2,7 +2,7 @@ var web_url = window.location.href
 var url = new URL(web_url)
 var redirectLink = url.searchParams.get("redirect")
 while (check-recaptcha-complete){
-    if(grecaptcha.getResponse().length !== 0){
+    if(grecaptcha.getResponse().length == 484){
         console.log("The captcha has been already solved");
     }
 }
