60f7a9cc73e928a10cc66a3888a5e6995144322f
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-02-24 19:33:57 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,17 @@
+/*
+(c) COPYRIGHT 2021 Salty Fish LLC Corporation. All rights reserved.
+*/
+
+/*
+Heya, my friend!
+We are very happy to meet you, but I'm curious where do you get these?
+If you get this by an operator or someone, we are very happy that you can help us,
+But if not, I hope that you can respect for our hard working and copyrights :D
+Have a nice day!
+
+Sincerely, Salty Fish Team :)
+*/
+function editPageResponseWithNoRecaptchaCompleted(e){
+    var errorTexts = document.getElementById("errorText")
+    errorTexts.innerHTML = "Please complete the reCAPTCHA above"
+}
--- script/redirect.js
+++ script/redirect.js
@@ -1,3 +1,16 @@
+/*
+(c) COPYRIGHT 2021 Salty Fish LLC Corporation. All rights reserved.
+*/
+
+/*
+Heya, my friend!
+We are very happy to meet you, but I'm curious where do you get these?
+If you get this by an operator or someone, we are very happy that you can help us,
+But if not, I hope that you can respect for our hard working and copyrights :D
+Have a nice day!
+
+Sincerely, Salty Fish Team :)
+*/
 var web_url = window.location.href
 var url = new URL(web_url)
 var redirectLink = url.searchParams.get("redirect")
@@ -13,6 +26,6 @@ function checkIfFinish(e){
         redirectPage()
     }
     else {
-        alert("Please complete the reCAPTCHA to continue")
+        editPageResponseWithNoRecaptchaCompleted()
     }
 }
